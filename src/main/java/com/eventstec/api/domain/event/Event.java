a801835f2e44bb6c481bca34f92ea3c7a422c2d0
Maycon <mayconprogramador@gmail.com>
2026-09-16 17:10:45 -0300

    refactor: remove unused imports

--- src/main/java/com/eventstec/api/domain/event/Event.java
+++ src/main/java/com/eventstec/api/domain/event/Event.java
@@ -1,18 +1,15 @@
 package com.eventstec.api.domain.event;
 
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 import com.eventstec.api.domain.address.Address;
-import com.eventstec.api.domain.coupon.Coupon;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
